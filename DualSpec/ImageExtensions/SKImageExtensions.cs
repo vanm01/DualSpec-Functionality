bc7c8788ecfba8ecb38e94220fe383b01f58859c
Vandad Mossavand <vm88@uakron.edu>
2021-07-06 09:56:33 -0400

    Intensity Measurements Added
    Pixel intensities can be calculated and displayed through the console

--- DualSpec/ImageExtensions/SKImageExtensions.cs
+++ DualSpec/ImageExtensions/SKImageExtensions.cs
@@ -1,6 +1,8 @@
-﻿using SkiaSharp;
+﻿using System;
+using System.Collections.Generic;
+using SkiaSharp;
 
-namespace DualSpec.Views
+namespace DualSpec
 {
     public static class SKImageExtensions
     {
@@ -11,6 +13,59 @@ namespace DualSpec.Views
             SKPixmap pixmap = image.PeekPixels();
 
             return pixmap;
+
+        }
+
+        public static unsafe void IntensityAtPixel(this SKImage processImage, int pixelCoordinates)
+        {
+
+            SKPixmap pixmap = processImage.PeekPixels();
+            byte* bmpPtr = (byte*)pixmap.GetPixels().ToPointer();
+            int width = processImage.Width;
+            int height = processImage.Height;
+            byte* tempPtr;
+
+            for (int row = 0; row < height; row++)
+            {
+
+                List <double> intensityList = new List <double>();
+
+
+                for (int column = 0; column < width; column ++)
+                {
+                    
+
+                    tempPtr = bmpPtr;
+                    byte red = *bmpPtr++;
+                    byte green = *bmpPtr++;
+                    byte blue = *bmpPtr++;
+                    byte alpha = *bmpPtr++;
+
+                    double intensity = (0.299 * red + 0.587 * green + 0.114 * blue);
+
+                    if(row == pixelCoordinates)
+                    {
+                        intensityList.Add(intensity);
+                    }
+                    
+
+                }
+
+                if(row == pixelCoordinates)
+                {
+                    double average = 0;
+
+                    for(int i = 0; i < intensityList.Count; i++)
+                    {
+                        average += intensityList[i];
+                    }
+
+                    average = average / (intensityList.Count);
+
+                    Console.WriteLine(average);
+                }
+
+            }
 
         }
         
---
+++ DualSpec/Views/IntensityReadingPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SkiaSharp;
+using Xamarin.Forms;
+
+namespace DualSpec.Views
+{
+    public partial class IntensityReadingPage : ContentPage
+    {
+
+        SKBitmap bitmap;
+        SKImage image;
+
+        public IntensityReadingPage()
+        {
+            InitializeComponent();
+        }
+
+        async void LoadImage_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            Stream stream = await DependencyService.Get<IPhotoPickerService>().PickPhotoAsync();
+
+
+            if (stream != null)
+            {
+                bitmap = SKBitmap.Decode(stream);
+            }
+
+            image = SKImage.FromBitmap(bitmap);
+
+            canvasView.InvalidateSurface();
+
+        }
+
+        void canvasView_PaintSurface(System.Object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs args)
+        {
+
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
+
+
+
+            canvas.Clear();
+
+            canvas.RotateDegrees(90);
+
+
+            if (bitmap != null)
+            {
+                canvas.DrawBitmap(bitmap, canvas.LocalClipBounds, null);
+            }
+
+        }
+
+        void Process_Clicked(System.Object sender, System.EventArgs e)
+        {
+
+            image.IntensityAtPixel(500);
+
+        }
+    }
+}
