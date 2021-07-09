12a661fd0697116438eea566026ff6742d33c682
Vandad Mossavand <55029866+vanm01@users.noreply.github.com>
2021-07-09 00:11:53 -0400

    Android Interface Implementations Finished
    Add the interface implementations for TouchEffects and PhotPickerService dependencies to get android project running

--- DualSpec.Android/MainActivity.cs
+++ DualSpec.Android/MainActivity.cs
@@ -4,6 +4,10 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using System.IO;
+using System.Threading.Tasks;
+using Android.Content;
+
 
 namespace DualSpec.Droid
 {
@@ -13,16 +17,42 @@ namespace DualSpec.Droid
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Instance = this;
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+
+        public TaskCompletionSource<Stream> PickImageTaskCompletionSource { set; get; }
+        internal static MainActivity Instance { get; private set; }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public static readonly int PickImageId = 1000;
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            base.OnActivityResult(requestCode, resultCode, intent);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (intent != null))
+                {
+                    Android.Net.Uri uri = intent.Data;
+                    Stream stream = ContentResolver.OpenInputStream(uri);
+
+                    // Set the Stream as the completion of the Task
+                    PickImageTaskCompletionSource.SetResult(stream);
+                }
+                else
+                {
+                    PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
         }
     }
 }
